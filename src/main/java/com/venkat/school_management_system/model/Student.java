454f3c1f49870959cb677caec442638eb3acb83d
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2021-10-19 16:40:46 -0500

    Update Student.java

--- src/main/java/com/venkat/school_management_system/model/Student.java
+++ src/main/java/com/venkat/school_management_system/model/Student.java
@@ -1,6 +1,8 @@
 package com.venkat.school_management_system.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,6 +10,7 @@ import javax.persistence.Table;
 @Table(name = "STUDENT")
 public class Student {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String firstName;
 	private String LastName;
